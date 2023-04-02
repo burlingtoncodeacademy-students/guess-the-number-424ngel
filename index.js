d07d2a1ffff4dec1680b7c91df03d56320efff5c
424ngel <angel.aguilarestra@gmail.com>
2023-04-02 15:24:07 -0400

    Implemented game logic (w/o input validation)

--- index.js
+++ index.js
@@ -7,12 +7,40 @@ function ask(questionText) {
   });
 }
 
-start();
+
 
 async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
+  let min = 0
+  let max = 101
+  let guessCount = 0
+  
+  while (guessCount < 7) {
+    let guess = Math.floor((min + max) / 2)
+    let check = await ask(`I guess ${guess}! Is that right? ('Y' or 'N') `)
+    guessCount++
+    if (check == 'y') {
+      console.log(`I WIN! I guessed your number with ${guessCount} guesses!`)
+      break
+    } else {
+      let highOrLow = await ask('Is it higher (H), or lower (L)? ')
+        if (highOrLow == 'h') {
+          min = guess + 1
+        } else if (highOrLow == 'l') {
+          max = guess - 1
+        }
+    }
+    
+  }
+  
+  if (guessCount > 7) {
+    console.log("I couldn't guess your number :(")
+  }
   process.exit();
 }
+
+
+start()
+
